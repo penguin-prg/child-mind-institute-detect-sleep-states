1f0308a551e6dca1b17a943af47fd257e29c78a4
penguin-prg <ryotapro2119@gmail.com>
2023-09-18 01:00:34 +0000

    metric

---
+++ utils/metric.py
@@ -0,0 +1,142 @@
+import numpy as np
+import pandas as pd
+import pandas.api.types
+from typing import Dict, List, Tuple
+
+# tolerances in steps
+tolerances = {
+    "onset": [12, 36, 60, 90, 120, 150, 180, 240, 300, 360],
+    "wakeup": [12, 36, 60, 90, 120, 150, 180, 240, 300, 360],
+}
+
+series_id_column_name = "series_id"
+time_column_name = "step"
+event_column_name = "event"
+score_column_name = "score"
+use_scoring_intervals = None
+
+
+def compute_comptetition_metric(
+    solution: pd.DataFrame,
+    submission: pd.DataFrame,
+    tolerances: Dict[str, List[float]] = tolerances,
+) -> float:
+    assert solution[time_column_name].isna().sum() == 0
+
+    # Ensure solution and submission are sorted properly
+    solution = solution.sort_values([series_id_column_name, time_column_name])
+    submission = submission.sort_values([series_id_column_name, time_column_name])
+
+    # Extract ground-truth events.
+    ground_truths = solution.query("event not in ['start', 'end']").reset_index(drop=True)
+
+    # 各イベントの出現回数
+    class_counts = ground_truths.value_counts(event_column_name).to_dict()
+
+    # Create table for detections with a column indicating a match to a ground-truth event
+    detections_filtered = submission.assign(matched=False)
+
+    # event-class x tolerance x series_id
+    aggregation_keys = pd.DataFrame(
+        [
+            (ev, tol, vid)
+            for ev in tolerances.keys()
+            for tol in tolerances[ev]
+            for vid in ground_truths[series_id_column_name].unique()
+        ],
+        columns=[event_column_name, "tolerance", series_id_column_name],
+    )
+
+    # Create match evaluation groups: event-class x tolerance x series_id
+    detections_grouped = aggregation_keys.merge(
+        detections_filtered, on=[event_column_name, series_id_column_name], how="left"
+    ).groupby([event_column_name, "tolerance", series_id_column_name])
+    ground_truths_grouped = aggregation_keys.merge(
+        ground_truths, on=[event_column_name, series_id_column_name], how="left"
+    ).groupby([event_column_name, "tolerance", series_id_column_name])
+    # Match detections to ground truth events by evaluation group
+    detections_matched = []
+    for key in aggregation_keys.itertuples(index=False):
+        dets = detections_grouped.get_group(key)
+        gts = ground_truths_grouped.get_group(key)
+        detections_matched.append(match_detections(dets["tolerance"].iloc[0], gts, dets))
+    detections_matched = pd.concat(detections_matched)
+
+    # Compute AP per event x tolerance group
+    event_classes = ground_truths[event_column_name].unique()
+    ap_table = (
+        detections_matched.query("event in @event_classes")
+        .groupby([event_column_name, "tolerance"])
+        .apply(
+            lambda group: average_precision_score(
+                group["matched"].to_numpy(),
+                group[score_column_name].to_numpy(),
+                class_counts[group[event_column_name].iat[0]],
+            )
+        )
+    )
+    # Average over tolerances, then over event classes
+    mean_ap = ap_table.groupby(event_column_name).mean().sum() / len(event_classes)
+
+    return mean_ap, ap_table
+
+
+def match_detections(tolerance: float, ground_truths: pd.DataFrame, detections: pd.DataFrame) -> pd.DataFrame:
+    """Match detections to ground truth events. Arguments are taken from a common event x tolerance x series_id evaluation group."""
+    detections_sorted = detections.sort_values(score_column_name, ascending=False).dropna()
+    is_matched = np.full_like(detections_sorted[event_column_name], False, dtype=bool)
+    gts_matched = set()
+    for i, det in enumerate(detections_sorted.itertuples(index=False)):
+        best_error = tolerance
+        best_gt = None
+
+        for gt in ground_truths.itertuples(index=False):
+            error = abs(getattr(det, time_column_name) - getattr(gt, time_column_name))
+            if error < best_error and gt not in gts_matched:
+                best_gt = gt
+                best_error = error
+
+        if best_gt is not None:
+            is_matched[i] = True
+            gts_matched.add(best_gt)
+
+    detections_sorted["matched"] = is_matched
+
+    return detections_sorted
+
+
+def precision_recall_curve(
+    matches: np.ndarray, scores: np.ndarray, p: int
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    if len(matches) == 0:
+        return [1], [0], []
+
+    # Sort matches by decreasing confidence
+    idxs = np.argsort(scores, kind="stable")[::-1]
+    scores = scores[idxs]
+    matches = matches[idxs]
+
+    distinct_value_indices = np.where(np.diff(scores))[0]
+    threshold_idxs = np.r_[distinct_value_indices, matches.size - 1]
+    thresholds = scores[threshold_idxs]
+
+    # Matches become TPs and non-matches FPs as confidence threshold decreases
+    tps = np.cumsum(matches)[threshold_idxs]
+    fps = np.cumsum(~matches)[threshold_idxs]
+
+    precision = tps / (tps + fps)
+    precision[np.isnan(precision)] = 0
+    recall = tps / p  # total number of ground truths might be different than total number of matches
+
+    # Stop when full recall attained and reverse the outputs so recall is non-increasing.
+    last_ind = tps.searchsorted(tps[-1])
+    sl = slice(last_ind, None, -1)
+
+    # Final precision is 1 and final recall is 0
+    return np.r_[precision[sl], 1], np.r_[recall[sl], 0], thresholds[sl]
+
+
+def average_precision_score(matches: np.ndarray, scores: np.ndarray, p: int) -> float:
+    precision, recall, _ = precision_recall_curve(matches, scores, p)
+    # Compute step integral
+    return -np.sum(np.diff(recall) * np.array(precision)[:-1])
