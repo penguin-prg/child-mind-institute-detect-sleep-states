8523e7aaecaceebca686d2f7180fc2967e6e7cab
penguin-prg <ryotapro2119@gmail.com>
2023-10-05 10:04:55 +0000

    wavenet baseline

--- src/utils/lightning_utils.py
+++ src/utils/lightning_utils.py
@@ -45,3 +45,86 @@ class MyLightningDataModule(pl.LightningDataModule):
         # dataloaderでnum_workers>1の時の乱数設定
         # これを指定しないと各workerのrandom_stateが同じになり、データも同じになる。
         np.random.seed(np.random.get_state()[1][0] + worker_id)
+
+
+class MyLightningModule(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.val_step_outputs = []
+        self.val_step_labels = []
+
+    def training_step(self, batch, batch_idx):
+        X, y = batch
+        preds = self.forward(X)
+
+        loss = self.loss_fn(preds, y)
+
+        self.train_metrics(preds, y)
+        self.log(
+            "loss",
+            loss,
+            prog_bar=True,
+            logger=True,
+            on_epoch=True,
+            on_step=True,
+        )
+        self.log_dict(
+            self.train_metrics,
+            prog_bar=True,
+            logger=True,
+            on_epoch=True,
+            on_step=True,
+        )
+        return {"loss": loss}
+
+    def validation_step(self, batch, batch_idx):
+        X, y = batch
+        preds = self.forward(X)
+
+        self.val_step_outputs.append(preds)
+        self.val_step_labels.append(y)
+
+    def on_validation_epoch_end(self):
+        preds = torch.cat(self.val_step_outputs)
+        labels = torch.cat(self.val_step_labels)
+        self.val_step_outputs.clear()
+        self.val_step_labels.clear()
+        loss = self.loss_fn(preds, labels)
+
+        self.valid_metrics(preds, labels)
+        self.log(
+            "val_loss",
+            loss,
+            prog_bar=False,
+            logger=True,
+            on_epoch=True,
+            on_step=False,
+        )
+        self.log_dict(
+            self.valid_metrics,
+            prog_bar=False,
+            logger=True,
+            on_epoch=True,
+            on_step=False,
+        )
+
+        # ログをprint
+        self.print_metric(preds, labels, "valid")
+
+    def print_metric(self, y_hat, y, train_or_valid="train"):
+        """
+        ログをprintする。次のepochが終わると上書きされてしまうので。
+        TODO: たぶんもっとマシな方法があるので探す。
+        """
+        if train_or_valid == "train":
+            metrics = self.train_metrics
+        else:
+            metrics = self.valid_metrics
+        loss = self.loss_fn(y_hat, y)
+
+        print(f"[epoch {self.trainer.current_epoch}] {train_or_valid}: ", end="")
+        print(f"{type(self.loss_fn).__name__}={loss:.4f}", end=", ")
+        for name in metrics:
+            v = metrics[name](y_hat, y)
+            print(f"{name}={v:.4f}", end=", ")
+        print()
--- src/utils/torch_models.py
+++ src/utils/torch_models.py
@@ -1,5 +1,6 @@
 from torch import nn
 import torch
+import torch.nn.functional as F
 
 
 class Conv1dBnRelu(nn.Module):
@@ -23,3 +24,35 @@ class Conv1dBnRelu(nn.Module):
         x = self.bn(x)
         x = self.relu(x)
         return x
+
+
+class WaveBlock(nn.Module):
+    """from https://www.kaggle.com/hanjoonchoe/wavenet-lstm-pytorch-ignite-ver"""
+
+    def __init__(self, in_channels, out_channels, dilation_rates):
+        super(WaveBlock, self).__init__()
+        self.num_rates = dilation_rates
+        self.convs = nn.ModuleList()
+        self.filter_convs = nn.ModuleList()
+        self.gate_convs = nn.ModuleList()
+
+        self.convs.append(nn.Conv1d(in_channels, out_channels, kernel_size=1))
+        dilation_rates = [2**i for i in range(dilation_rates)]
+        for dilation_rate in dilation_rates:
+            self.filter_convs.append(
+                nn.Conv1d(out_channels, out_channels, kernel_size=3, padding=dilation_rate, dilation=dilation_rate)
+            )
+            self.gate_convs.append(
+                nn.Conv1d(out_channels, out_channels, kernel_size=3, padding=dilation_rate, dilation=dilation_rate)
+            )
+            self.convs.append(nn.Conv1d(out_channels, out_channels, kernel_size=1))
+
+    def forward(self, x):
+        x = self.convs[0](x)
+        res = x
+        for i in range(self.num_rates):
+            x = F.tanh(self.filter_convs[i](x)) * F.sigmoid(self.gate_convs[i](x))
+            x = self.convs[i + 1](x)
+            # x += res
+            res = torch.add(res, x)
+        return res
