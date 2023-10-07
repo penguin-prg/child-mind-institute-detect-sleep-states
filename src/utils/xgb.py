340622a2545fc9f239cfefd8072b4988927e0e78
penguin-prg <ryotapro2119@gmail.com>
2023-10-07 13:55:02 +0000

    debug3

--- src/utils/xgb.py
+++ src/utils/xgb.py
@@ -1,4 +1,4 @@
-import xgboost as xgb
+import lightgbm as lgbm
 from tqdm import tqdm
 import numpy as np
 import pandas as pd
@@ -27,31 +27,31 @@ def fit_xgb(
             print(f"== fold {i} ==")
         trn_idx = folds != i
         val_idx = folds == i
-        dtrain = xgb.DMatrix(X[features][trn_idx], label=y[trn_idx], enable_categorical=True)
-        dvalid = xgb.DMatrix(X[features][val_idx], label=y[val_idx], enable_categorical=True)
-        evallist = [(dvalid, "eval")]
+        X_train, y_train = X[features][trn_idx], y[trn_idx]
+        X_valid, y_valid = X[features][val_idx], y[val_idx]
 
-        model = xgb.train(
-            params,
-            dtrain,
-            num_boost_round=1000000,
-            evals=evallist,
+        model = lgbm.LGBMClassifier(**params)
+        model.fit(
+            X_train,
+            y_train,
+            eval_set=[(X_valid, y_valid)],
             early_stopping_rounds=es_rounds,
-            verbose_eval=verbose,
+            eval_metric="logloss",
+            verbose=verbose,
         )
-        valid_pred = model.predict(dvalid)
+
+        train_pred = model.predict_proba(X_train)[:, 1]
+        valid_pred = model.predict_proba(X_valid)[:, 1]
+
         oof[val_idx] = valid_pred
         models.append(model)
-
-        del dtrain, dvalid, valid_pred
+        del X_train, X_valid, y_train, y_valid, train_pred, valid_pred
         gc.collect()
 
     return oof, models
 
 
 def inference_xgb(models: list, feat_df: pd.DataFrame, pred_type: str = "regression"):
-    assert pred_type in ["regression", "binary"]
-    dtrain = xgb.DMatrix(feat_df, enable_categorical=True)
-    if pred_type == "regression":
-        pred = np.array([model.predict(dtrain) for model in models]).mean(axis=0)
+    pred = np.array([model.predict_proba(feat_df)[:, 1] for model in models])
+    pred = np.mean(pred, axis=0)
     return pred
